1675e371de1490c262c3fda6ec81a8933f5ebadf
zhangjiaxing <13880614578@163.com>
2021-06-03 19:35:55 +0800

    first

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by :
+ * User zjx
+ * Date 2021/6/3
+ * Time 下午 7:10
+ */
+
+require "vendor/autoload.php";
+
+
+$Test = new Siam\Test();
+$Test->test();
---
+++ src/siam/test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by :
+ * User zjx
+ * Date 2021/6/3
+ * Time 下午 7:14
+ */
+
+namespace Siam;
+
+class Test
+{
+    function test()
+    {
+        echo "Form Test -> test()";
+    }
+}
